bbba8a425b2ebbbe75a26db1e45dc0ad3c615366
Stepan Ivanov <147562256+StephnIvanov@users.noreply.github.com>
2023-12-11 05:07:52 +0300

    Create code.py

---
+++ Задание11/code.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+import requests
+import json
+
+def repos_inf(repo,label):
+    file = f'{repo}_output.json'
+    try:
+        filtered = {}
+        output = requests.get(f'https://api.github.com/users/{repo}').json()
+        needed = ['company','created_at','email','id','name','url']
+        for i in needed:
+            if i in output.keys() == False:
+                raise Exception('Такого репозитория не существует')
+            filtered[i] = output[i]
+        with open(file,'w') as write:
+            json.dump(filtered,write)
+        txt = f'Cохранено сюда: {file}'
+    except Exception as err:
+        txt = f'Erorr: {err}'
+    label.config(text=txt)
+
+def main_ui():
+    window = tk.Tk()
+    window.title('GitHub json parser')
+
+    repo = tk.Entry(window)
+    repo_btn = tk.Button(window, text='Parse', command=lambda: repos_inf(repo.get(),output))
+    txt = tk.Label(window, text='Укажите имя репозитория')
+    output = tk.Label(window)
+
+    modules = [txt,repo,parse_btn,output]
+
+    for i in modules:
+        i.pack(pady=10)
+
+    window.mainloop()
+
+main_ui()
